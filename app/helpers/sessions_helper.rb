d10f7bb3f1dcbd84147af25f150dcc3f6559b5df
Ruairi O'Ciarain <ruairi@Ruairis-MBP.lan>
2022-01-16 22:51:52 +0000

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,8 +9,10 @@ module SessionsHelper
       # Returns the current logged-in user (if any).
       def current_user
         if (user_id = session[:user_id])
-            user = User.find_by(id: user_id)
-            @current_user ||= user if session[:session_token] == user.session_token
+          user = User.find_by(id: user_id)
+          if user && session[:session_token] == user.session_token
+            @current_user = user
+          end
         elsif (user_id = cookies.encrypted[:user_id])
           user = User.find_by(id: user_id)
           if user && user.authenticated?(cookies[:remember_token])
@@ -40,5 +42,13 @@ module SessionsHelper
         user.remember
         cookies.permanent.encrypted[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
+    end
+
+    def current_user?(user)
+      user && user == current_user
+    end
+
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
     end
 end
