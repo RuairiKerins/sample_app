b583d14615a429006bdf1bd4ebf67da70c26257a
Ruairi O'Ciarain <ruairi@Ruairis-MBP.lan>
2022-01-17 10:46:47 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.raise_delivery_errors = false
+
+  host = 'localhost:3000' #local host
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
